7967ab096489b2af1f7186245131ab63b7a75041
01emastaff <matsufuji@emanet.jp>
2026-02-16 18:03:29 +0900

    # 002:feature: 画像追加

--- public/rcs_seijin/_assets/js/script.js
+++ public/rcs_seijin/_assets/js/script.js
@@ -3,3 +3,23 @@
 * SCRIPT JS
 *
 ***************************************************************************/
+document.addEventListener('DOMContentLoaded', () => {
+
+  const targets = document.querySelectorAll('.move');
+
+  const observer = new IntersectionObserver((entries, observer) => {
+	entries.forEach(entry => {
+
+	  if (entry.isIntersecting) {
+		entry.target.classList.add('is-show');
+		observer.unobserve(entry.target);
+	  }
+
+	});
+  }, {
+	threshold: 0.2
+  });
+
+  targets.forEach(target => observer.observe(target));
+
+});
