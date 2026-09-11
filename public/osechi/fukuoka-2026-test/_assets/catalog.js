8bd508c28707fce2fd87da3d2a9b85e4f7b92a53
syu5687 <mk@emanet.jp>
2026-09-11 16:58:02 +0900

    Label osechi pickup locations as delivery available

--- public/osechi/fukuoka-2026-test/_assets/catalog.js
+++ public/osechi/fukuoka-2026-test/_assets/catalog.js
@@ -175,7 +175,7 @@ function pickupGuide(category){
   const categories=category==='all'?Object.keys(categoryNames):[category];
   return categories.map(key=>{
     const locations=[...new Set(catalogProducts.filter(p=>p.category===key).flatMap(p=>p.pickup))];
-    return `<div class="pickup-location-group"><h3>${categoryNames[key]} 受け取り可能店舗 <span>${locations.length}店舗</span></h3><ul>${locations.map(name=>`<li>${name}</li>`).join('')}</ul></div>`;
+    return `<div class="pickup-location-group"><h3>${categoryNames[key]} 受け取り可能店舗 <span>${locations.length}店舗</span>${key==='osechi'?'<b class="delivery-badge">配達可</b>':''}</h3><ul>${locations.map(name=>`<li>${name}</li>`).join('')}</ul></div>`;
   }).join('');
 }
 function filterCatalog(category){
